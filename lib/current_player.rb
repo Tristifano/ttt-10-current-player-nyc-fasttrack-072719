efb960c4475e843af6425c9a7821d0c8105a444b
Tristan Myers <tristifano@gmail.com>
2019-07-28 18:03:41 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,12 +1,10 @@
 
 def turn_count(board)
   counter = 0
-  board.each do |position|
-    if position == "X" || "O"
-      counter += 1
+  for position in board
+    if position == "O" || "X"
+      counter += 1 
     else 
-      counter += 0 
-    end
-  end
+      counter += 0
   return counter
 end
